181b63caf656d5170fc36da086a3c257fc406edb
三股猛 <takeru@mimatatakeshinoMacBook-Pro.local>
2019-03-17 05:29:38 +0900

    メッセージ

---
+++ database/migrations/2019_03_16_202055_create_event_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->bigIncrements('id');  //イベントid
+            $table->double('longitude');  //緯度
+            $table->double('latitude');   //経度
+            $table->integer('user_id');   //ユーザーid
+            $table->timestamps();         //登録日時
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+}
