d0b54080b604b25f8148be271d6512d3542a9506
三股猛 <takeru@mimatatakeshinoMacBook-Pro.local>
2019-03-17 01:45:23 +0900

    メッセージ

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,33 +11,33 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('facebooo', function (Blueprint $table) {
-          $table->increments('id');
-          $table->string('name');
-          $table->string('email',191)->unique()->nullable();
-          $table->string('prof_image')->default('imag.png');   //デフォルトを設定しておく必要あり
-          $table->year('birth_year')->nullable();
-          $table->integer('birth_month')->nullable();
-          $table->integer('birth_day')->nullable();
-          $table->integer('point')->default(0);
-          $table->integer('sex')->nullable();
-          $table->string('cast_or_gest')->nullable();
-          $table->timestamp('email_verified_at')->nullable();
-          $table->string('password')->nullable();
-          $table->rememberToken();
-          $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('facebooo');
-    }
+    // public function up()
+    // {
+    //     Schema::create('facebooo', function (Blueprint $table) {
+    //       $table->increments('id');
+    //       $table->string('name');
+    //       $table->string('email',191)->unique()->nullable();
+    //       $table->string('prof_image')->default('imag.png');   //デフォルトを設定しておく必要あり
+    //       $table->year('birth_year')->nullable();
+    //       $table->integer('birth_month')->nullable();
+    //       $table->integer('birth_day')->nullable();
+    //       $table->integer('point')->default(0);
+    //       $table->integer('sex')->nullable();
+    //       $table->string('cast_or_gest')->nullable();
+    //       $table->timestamp('email_verified_at')->nullable();
+    //       $table->string('password')->nullable();
+    //       $table->rememberToken();
+    //       $table->timestamps();
+    //     });
+    // }
+    //
+    // /**
+    //  * Reverse the migrations.
+    //  *
+    //  * @return void
+    //  */
+    // public function down()
+    // {
+    //     Schema::dropIfExists('facebooo');
+    // }
 }
---
+++ database/migrations/2019_03_16_164235_create_facebook_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacebookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facebook', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->string('email',191)->unique()->nullable();
+          $table->string('prof_image')->default('imag.png');   //デフォルトを設定しておく必要あり
+          $table->year('birth_year')->nullable();
+          $table->integer('birth_month')->nullable();
+          $table->integer('birth_day')->nullable();
+          $table->integer('point')->default(0);
+          $table->integer('sex')->nullable();
+          $table->string('cast_or_gest')->nullable();
+          $table->timestamp('email_verified_at')->nullable();
+          $table->string('password')->nullable();
+          $table->rememberToken();
+          $table->timestamps();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facebook');
+    }
+}
