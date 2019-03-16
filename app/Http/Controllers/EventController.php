43a0d3f0a9d0cf1ece8c1500e9354090af2feaad
三股猛 <takeru@mimatatakeshinoMacBook-Pro.local>
2019-03-17 06:52:25 +0900

    メッセージ

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -28,7 +28,7 @@ class EventController extends Controller
       //$patocall->wether_valid =1;
       $event->save();
 
-      $id = Event::where(id=$event->id)->first();
+      $id = Event::where(id == $event->id)->first();
       if($id){
         $event = Event::all();
         return $event;
