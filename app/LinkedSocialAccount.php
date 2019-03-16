4c88e7bba0c8fb463297fa4d5fb446e5cc55dd17
三股猛 <takeru@mimatatakeshinoMacBook-Pro.local>
2019-03-17 04:07:50 +0900

    メッセージ

--- app/LinkedSocialAccount.php
+++ app/LinkedSocialAccount.php
@@ -8,4 +8,12 @@ class LinkedSocialAccount extends Model
 {
     //
     protected $table = 'linked_social_account';
+
+    protected $fillable = ['provider_name', 'provider_id' ];
+
+
+    public function user()
+{
+    return $this->belongsTo('App\User');
+}
 }
